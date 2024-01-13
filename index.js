7a0db8fe4efd4dbb474b32dcb37230cc12c4b453
Roland Levy <contact@rolandlevy.co.uk>
2024-01-13 20:19:48 +0000

    Added eslint and prettier config

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const app = express();
 require('dotenv').config();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,17 +18,21 @@ app.post('/search', async (req, res) => {
   const { movieTitle } = req.body;
 
   try {
-    const omdbUrl = `http://www.omdbapi.com/?t=${encodeURIComponent(movieTitle)}&apikey=${OMDB_API_KEY}`;
+    const omdbUrl = `http://www.omdbapi.com/?s=${encodeURIComponent(movieTitle)}&apikey=${OMDB_API_KEY}`;
     const response = await axios.get(omdbUrl);
     const movieData = response.data;
-    // console.log({movieData});
-
     if (movieData.Response === 'True') {
-      res.send(`
-        <h2>${movieData.Title}</h2>
-        <img src="${movieData.Poster}" alt="${movieData.Title} Poster">
-        <p>${movieData.Plot}</p>
-      `);
+      const movies = movieData.Search.map(item => {
+        // json data -> http://www.omdbapi.com/?i=${item.imdbID}&apikey=${OMDB_API_KEY}
+        return `
+          <section>
+            <h2>${item.Title}, ${item.Year}</h2>
+            <p><a href="https://www.imdb.com/title/${item.imdbID}" target="_blank">Link</a></p>
+            <img src="${item.Poster}" alt="${item.Title} Poster">
+          </section>
+        `;
+      }).join('');
+      res.send(`<article class="movies">${movies}</article>`);
     } else {
       res.send(`<p>No results found for "${movieTitle}".</p>`);
     }
